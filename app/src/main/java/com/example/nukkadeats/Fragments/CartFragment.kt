ba5df7e02f735f83c5224955d2b4604f0dc3b993
Ayushh0708 <paliwalaayush717@gmail.com>
2025-03-31 02:31:00 +0530

    Add 1 Activity(CartProceed) and 1 Fragment(OrderPlaced)
    and some other changes

--- app/src/main/java/com/example/nukkadeats/Fragments/CartFragment.kt
+++ app/src/main/java/com/example/nukkadeats/Fragments/CartFragment.kt
@@ -1,14 +1,18 @@
 package com.example.nukkadeats.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.nukkadeats.CartProceed
+import com.example.nukkadeats.OrderPlaced
 import com.example.nukkadeats.R
 import com.example.nukkadeats.adapters.cartAdapter
 import com.example.nukkadeats.databinding.FragmentCartBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class CartFragment : Fragment() {
     private lateinit var binding : FragmentCartBinding
@@ -32,6 +36,12 @@ class CartFragment : Fragment() {
         val adapter = cartAdapter(ArrayList(foodNames) , ArrayList(foodPrice) , ArrayList(foodImage))
         binding.cartRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.cartRecyclerView.adapter = adapter
+
+
+        binding.proceedBtn.setOnClickListener {
+            val intent = Intent(requireContext() , CartProceed::class.java)
+            startActivity(intent)
+        }
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/nukkadeats/OrderPlaced.kt
@@ -0,0 +1,40 @@
+package com.example.nukkadeats
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nukkadeats.Fragments.HomeFragment
+import com.example.nukkadeats.databinding.FragmentOrderPlacedBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class OrderPlaced : BottomSheetDialogFragment() {
+
+    private lateinit var binding : FragmentOrderPlacedBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentOrderPlacedBinding.inflate(inflater , container , false)
+
+        binding.goToHome.setOnClickListener {
+            val intent = Intent(requireContext() , MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            startActivity(intent)
+        }
+
+        return binding.root
+    }
+
+
+}
