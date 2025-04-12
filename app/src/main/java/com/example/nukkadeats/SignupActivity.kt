e6d204378d167a0dc756948a418eccd12135d70a
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-12 21:08:36 +0530

    Login and Signin Activity

--- app/src/main/java/com/example/nukkadeats/SignupActivity.kt
+++ app/src/main/java/com/example/nukkadeats/SignupActivity.kt
@@ -6,10 +6,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import androidx.core.view.WindowInsetsCompat
 import com.example.nukkadeats.databinding.ActivitySignupBinding
+import com.facebook.CallbackManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var username : String
+    private lateinit var email : String
+    private lateinit var password : String
+    private lateinit var auth : FirebaseAuth
+    private lateinit var database : DatabaseReference
+    private lateinit var googleSignInClient : GoogleSignInClient
+    private lateinit var callbackManager : CallbackManager
 
     private val binding : ActivitySignupBinding by lazy{
         ActivitySignupBinding.inflate(layoutInflater)
@@ -18,6 +33,12 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(binding.root)
+
+        //Initialize Firebase Auth
+        auth = Firebase.auth
+
+        //Inititalize Database
+        database = Firebase.database.reference
 
         binding.alreadyHaveAccount.setOnClickListener{
             onBackPressedDispatcher.onBackPressed()
