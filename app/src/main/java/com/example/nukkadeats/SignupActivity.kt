e46067e7a834e3157478a74326f15f3852bbcb8e
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-13 00:49:53 +0530

    GitIgnore update

--- app/src/main/java/com/example/nukkadeats/SignupActivity.kt
+++ app/src/main/java/com/example/nukkadeats/SignupActivity.kt
@@ -1,16 +1,22 @@
 package com.example.nukkadeats
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.example.nukkadeats.Modal.UserModal
 import com.example.nukkadeats.databinding.ActivitySignupBinding
 import com.facebook.CallbackManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
@@ -40,8 +46,23 @@ class SignupActivity : AppCompatActivity() {
         //Inititalize Database
         database = Firebase.database.reference
 
+        //
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.Default_web_client_id)).requestEmail().
+            build()
+
+        //Google Initialize
+        googleSignInClient = GoogleSignIn.getClient(this , googleSignInOptions)
+
+        //Already have account
         binding.alreadyHaveAccount.setOnClickListener{
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        //Google Button
+        binding.googleButton.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            launcher.launch(signInIntent)
         }
 
         binding.signUpCreateButton.setOnClickListener{
@@ -64,6 +85,33 @@ class SignupActivity : AppCompatActivity() {
         }
     }
 
+    //Launcher for Google
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            if(task.isSuccessful){
+                val account : GoogleSignInAccount? = task.result
+                val credential = GoogleAuthProvider.getCredential(account?.idToken , null)
+
+                auth.signInWithCredential(credential).addOnCompleteListener {authTask->
+                    if(task.isSuccessful){
+                        val user = auth.currentUser
+                        Toast.makeText(this , "Welcome ${user?.displayName}" , Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this , MainActivity::class.java))
+                        finish()
+                    }else{
+                        Toast.makeText(this , "Account creation failed" , Toast.LENGTH_LONG).show()
+                        Log.d("AccountFail" , "createAccount: ${task.exception}")
+                    }
+                }
+            }else{
+                Toast.makeText(this , "Account creation failed" , Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    //Account creation with Email and Password
     private fun createAccount(email : String , password : String) {
         auth.createUserWithEmailAndPassword(email , password).addOnCompleteListener { task ->
 
