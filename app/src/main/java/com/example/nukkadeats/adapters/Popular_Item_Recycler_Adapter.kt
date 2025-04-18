fe307ef33c21e59d6391efb196a1783b73f6c374
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-18 19:42:11 +0530

    Working on Food data into from NukkadEatsAdmin

--- app/src/main/java/com/example/nukkadeats/adapters/Popular_Item_Recycler_Adapter.kt
+++ app/src/main/java/com/example/nukkadeats/adapters/Popular_Item_Recycler_Adapter.kt
@@ -8,10 +8,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.nukkadeats.ItemDetailsActivity
 import com.example.nukkadeats.databinding.PopularItemRecyclerBinding
 
-class Popular_Item_Recycler_Adapter(private val itemsName : List<String>, private val itemPrice : List<String> ,private val itemImage : List<Int> , private val itemDescription : List<String> , private val context : Context) : RecyclerView.Adapter<Popular_Item_Recycler_Adapter.ViewHolder>() {
-    class ViewHolder (private val binding: PopularItemRecyclerBinding) : RecyclerView.ViewHolder(binding.root) {
-        private val imgView =  binding.popularFoodImage
-        fun bind(nameItem: String, img: Int, priceItem: String , descriptionItem: String){
+class Popular_Item_Recycler_Adapter(
+    private val itemsName: List<String>,
+    private val itemPrice: List<String>,
+    private val itemImage: List<Int>,
+    private val itemDescription: List<String>,
+    private val context: Context
+) : RecyclerView.Adapter<Popular_Item_Recycler_Adapter.ViewHolder>() {
+    class ViewHolder(private val binding: PopularItemRecyclerBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private val imgView = binding.popularFoodImage
+        fun bind(nameItem: String, img: Int, priceItem: String, descriptionItem: String) {
             binding.popularFoodName.text = nameItem
             binding.popularFoodPrice.text = priceItem
             binding.popularFoodDescription.text = descriptionItem
@@ -21,7 +28,13 @@ class Popular_Item_Recycler_Adapter(private val itemsName : List<String>, privat
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(PopularItemRecyclerBinding.inflate(LayoutInflater.from(parent.context) , parent , false))
+        return ViewHolder(
+            PopularItemRecyclerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun getItemCount(): Int {
@@ -34,12 +47,12 @@ class Popular_Item_Recycler_Adapter(private val itemsName : List<String>, privat
         val priceItem = itemPrice[position]
         val descriptionItem = itemDescription[position]
 
-        holder.bind(namesItem, imageItem, priceItem , descriptionItem)
+        holder.bind(namesItem, imageItem, priceItem, descriptionItem)
 
         holder.itemView.setOnClickListener {
-            val intent = Intent(context , ItemDetailsActivity::class.java)
-            intent.putExtra("MenuItemName" , namesItem)
-            intent.putExtra("MenuItemImage" , imageItem)
+            val intent = Intent(context, ItemDetailsActivity::class.java)
+            intent.putExtra("MenuItemName", namesItem)
+            intent.putExtra("MenuItemImage", imageItem)
 
             context.startActivity(intent)
         }
