f290706b8989eb493f720e1ee22d725cb7329568
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-24 02:21:44 +0530

    Working on Cart Fragment

--- app/src/main/java/com/example/nukkadeats/Modal/CartItems.kt
+++ app/src/main/java/com/example/nukkadeats/Modal/CartItems.kt
@@ -1,9 +1,9 @@
 package com.example.nukkadeats.Modal
 
 data class CartItems(
-    var foodName : String ?= null,
-    var foodPrice : String ?= null,
-    var foodImage : String ?= null,
-    var foodDescription : String ?= null,
-    var foodQuantity : Int ?= null,
+    var foodName: String? = null,
+    var foodPrice: String? = null,
+    var foodImage: String? = null,
+    var foodDescription: String? = null,
+    var foodQuantity: Int? = null,
 )
