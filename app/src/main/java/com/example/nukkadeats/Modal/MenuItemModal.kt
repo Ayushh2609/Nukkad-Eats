e846507eb484db003f454b45be6b4457af38cd48
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-24 02:22:59 +0530

    Working on Cart Fragment

--- app/src/main/java/com/example/nukkadeats/Modal/MenuItemModal.kt
+++ app/src/main/java/com/example/nukkadeats/Modal/MenuItemModal.kt
@@ -1,9 +1,9 @@
 package com.example.nukkadeats.Modal
 
 data class MenuItemModal(
-    val foodName : String? = null,
-    val foodPrice : String? = null,
-    val foodDescription : String? = null,
-    val foodImage : String? = null,
-    val foodIngredient : String? = null
+    val foodName: String? = null,
+    val foodPrice: String? = null,
+    val foodDescription: String? = null,
+    val foodImage: String? = null,
+    val foodIngredient: String? = null
 )
