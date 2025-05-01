6b5eee107a1a34deffa7db0577f84c068c60efae
Ayushh0708 <paliwalaayush717@gmail.com>
2025-05-01 18:50:45 +0530

    Cart Proceed

--- app/src/main/java/com/example/nukkadeats/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/nukkadeats/Fragments/ProfileFragment.kt
@@ -1,14 +1,28 @@
 package com.example.nukkadeats.Fragments
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.nukkadeats.Modal.UserModal
 import com.example.nukkadeats.R
+import com.example.nukkadeats.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+    private val auth = FirebaseAuth.getInstance()
+    private val database = FirebaseDatabase.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,7 +32,73 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+
+        setUserData()
+
+        binding.saveInfoProfile.setOnClickListener {
+
+            val name = binding.nameProfile.text.toString()
+            val address = binding.addressProfile.text.toString()
+            val email = binding.emailProfile.text.toString()
+            val phone = binding.phoneProfile.text.toString()
+
+            updateUserData(name , address , email , phone)
+        }
+
+        return binding.root
+    }
+
+    private fun updateUserData(name: String, address: String, email: String, phone: String) {
+        val userId = auth.currentUser?.uid
+        if(userId != null){
+            val userRef = database.reference.child("users").child(userId)
+
+            val userData = hashMapOf(
+                "name" to name,
+                "address" to address,
+                "email" to email,
+                "phone" to phone
+            )
+            userRef.setValue(userData).addOnSuccessListener {
+                Toast.makeText(requireContext() , "Profile updated Successfully" , Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(requireContext() , "Profile updated failed" , Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setUserData() {
+        val userId = auth.currentUser?.uid
+
+        if (userId != null) {
+
+            val userRef = database.reference.child("users").child(userId)
+
+            userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+
+                        val userProfile = snapshot.getValue(UserModal::class.java)
+                        if(userProfile !=null){
+                            binding.nameProfile.setText(userProfile.name)
+                            binding.addressProfile.setText(userProfile.address)
+                            binding.emailProfile.setText(userProfile.email)
+                            binding.phoneProfile.setText(userProfile.phone)
+
+                        }
+
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+        }
+
+
     }
 }
--- app/src/main/java/com/example/nukkadeats/Modal/UserModal.kt
+++ app/src/main/java/com/example/nukkadeats/Modal/UserModal.kt
@@ -4,5 +4,7 @@ data class UserModal(
     val name : String? = null,
     val email : String? = null,
     val password : String? = null,
-    val loginMethod : String? = null
+    val loginMethod : String? = null,
+    val phone : String? = null,
+    val address : String? = null
 )
