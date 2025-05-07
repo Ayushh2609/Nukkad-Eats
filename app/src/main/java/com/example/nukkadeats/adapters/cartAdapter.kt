5b9b1d23fb4fe34ff23547f5fbc5268114d2399a
Ayushh0708 <paliwalaayush717@gmail.com>
2025-05-07 21:27:00 +0530

    Cart Proceed

--- app/src/main/java/com/example/nukkadeats/adapters/cartAdapter.kt
+++ app/src/main/java/com/example/nukkadeats/adapters/cartAdapter.kt
@@ -170,7 +170,6 @@ class cartAdapter(
         holder.bind(position)
     }
 
-
     //GET updated quantity
     fun getUpdatedItemsQuantities(): MutableList<Int> {
         val itemQuant = mutableListOf<Int>()
