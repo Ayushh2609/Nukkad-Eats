61a00d4c71c2a7ff4a2e450a62dbfc473d6edf1c
Ayushh0708 <paliwalaayush717@gmail.com>
2025-05-21 20:49:24 +0530

    Cart Proceed

--- app/src/main/java/com/example/nukkadeats/Modal/OrderDetaild.kt
+++ app/src/main/java/com/example/nukkadeats/Modal/OrderDetaild.kt
@@ -79,6 +79,7 @@ class OrderDetaild() : Parcelable {
         return 0
     }
 
+
     companion object CREATOR : Parcelable.Creator<OrderDetaild> {
         override fun createFromParcel(parcel: Parcel): OrderDetaild {
             return OrderDetaild(parcel)
