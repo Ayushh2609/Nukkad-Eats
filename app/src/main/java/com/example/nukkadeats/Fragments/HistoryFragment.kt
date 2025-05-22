d014493545a9e89f96fb8361a1dcd58b623d37f1
Ayushh0708 <paliwalaayush717@gmail.com>
2025-05-22 19:37:19 +0530

    History Fragment (Recent buy Items)

--- app/src/main/java/com/example/nukkadeats/Fragments/HistoryFragment.kt
+++ app/src/main/java/com/example/nukkadeats/Fragments/HistoryFragment.kt
@@ -1,19 +1,32 @@
 package com.example.nukkadeats.Fragments
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.nukkadeats.Modal.OrderDetaild
 import com.example.nukkadeats.R
 import com.example.nukkadeats.adapters.BuyAgainAdapter
 import com.example.nukkadeats.databinding.FragmentHistoryBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HistoryFragment : Fragment() {
 
     private lateinit var binding : FragmentHistoryBinding
     private lateinit var buyAgainAdapter: BuyAgainAdapter
+
+    private lateinit var database : FirebaseDatabase
+    private lateinit var auth : FirebaseAuth
+    private lateinit var userId : String
+    private var listOfOrderItems : MutableList<OrderDetaild> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +41,62 @@ class HistoryFragment : Fragment() {
         binding = FragmentHistoryBinding.inflate(layoutInflater , container , false)
         // Inflate the layout for this fragment
         setBuyAgainAdapter()
+
+        auth = FirebaseAuth.getInstance()
+
+        database = FirebaseDatabase.getInstance()
+
+        //Retreive Buy item history
+        retriveBuyHistory()
+
+
         return binding.root
+    }
+
+    private fun retriveBuyHistory() {
+        binding.recentlyBuyItem.visibility = View.GONE
+        userId = auth.currentUser?.uid?:""
+
+        val buyItemReference = database.getReference("Users").child(userId).child("OrderHistory")
+        val shortingQuery = buyItemReference.orderByChild("currentTime")
+
+        shortingQuery.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(buySnapShot in snapshot.children){
+                    val buyHistoryItem = buySnapShot.getValue(OrderDetaild ::class.java)
+                    buyHistoryItem?.let {
+                        listOfOrderItems.add(it)
+                    }
+                }
+
+                listOfOrderItems.reverse()
+
+                if(listOfOrderItems.isNotEmpty()){
+                    setDataInRecentBuyItem()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
+
+    private fun setDataInRecentBuyItem() {
+        binding.recentlyBuyItem.visibility = View.VISIBLE
+        val recentOrderItem = listOfOrderItems.firstOrNull()
+        recentOrderItem?.let {
+            with(binding){
+                recentOrderName.text = it.foodNames?.firstOrNull()?:""
+
+                val image = it.foodImages?.firstOrNull()?:""
+                val uri = Uri.parse(image)
+
+                Glide.with(requireContext()).load(uri).into(recentOrderImage)
+            }
+        }
     }
 
     private fun setBuyAgainAdapter() {
